ee0334342e4c396f1fab3096e540ee94fac78527
vimtaku <vimtaku@gmail.com>
2016-02-22 19:06:53 +0900

    Fix

--- Gemfile
+++ Gemfile
@@ -46,4 +46,5 @@ group :development do
   gem 'spring'
 end
 
+gem "slim-rails"
 gem 'ridgepole'
--- config/application.rb
+++ config/application.rb
@@ -1,42 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Chat
+module MithriljsRailsSample
   class Application < Rails::Application
-    I18n.enforce_available_locales = true
-    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
-    config.i18n.default_locale = :en
-
-    config.generators do |g|
-      g.template_engine :slim
-      g.javascripts false
-      g.stylesheets false
-      g.helper false
-      g.test_framework :rspec,
-        fixture: true,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false,
-        controller_specs: true
-      g.fixture_replacement :factory_girl, dir: "spec/factories"
-    end
-
-    config.autoload_paths << Rails.root.join("lib")
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
